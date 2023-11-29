12fe89f505f5d2aa86409bfd3dfdaa0af8836628
Dalarin1 <103304145+Dalarin1@users.noreply.github.com>
2023-11-29 20:56:52 +0500

    Update Laws.h

--- Laws.h
+++ Laws.h
@@ -1,12 +1,12 @@
 #pragma once
 #include "Planet.h"
 
-sf::Vector2f Fg(sf::Vector2f F, float m) { return F / m; } // получаем вектор силы, возвращаем вектор ускорения
+sf::Vector2f Fg(sf::Vector2f F, float m) { return F / m; } // get the force vector, return the acceleration vector
 
 sf::Vector2f A(Planet a, Planet b) {
-	sf::Vector2f dir = b.getPosition() - a.getPosition(); // dir - вектор, соединяющий планеты а и b
-	float r = sqrt(dir.x * dir.x + dir.y * dir.y); // r - расстояние между планетами
-	dir.x /= r; dir.y /= r;  // нормализуем вектор dir (делаем его длиной 1)
-	sf::Vector2f acceleration = Fg(dir * ((a.getMass() * b.getMass()) / (r * r)), a.getMass()); // задаем вектор ускорения
+	sf::Vector2f dir = b.getPosition() - a.getPosition(); // dir - vector connecting planets a and b
+	float r = sqrt(dir.x * dir.x + dir.y * dir.y); // r is the distance between the planets
+	dir.x /= r; dir.y /= r;  // normalize vector dir (make it with length 1)
+	sf::Vector2f acceleration = Fg(dir * ((a.getMass() * b.getMass()) / (r * r)), a.getMass()); // set the acceleration vector
 	return acceleration;
 }
